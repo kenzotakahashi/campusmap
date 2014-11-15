751865756d3f3be663d5958e8f4b06905e54f125
Kenzo Takahashi <kenzotakahashi2@gmail.com>
2014-11-14 21:03:30 -0600

    changed init

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,17 @@ from flask import Flask
 from pymongo import MongoClient
 import os
 
+# Format: MONGOHQ_URL: mongodb://<user>:<pass>@<base_url>:<port>/<url_path>
+if os.environ.get('MONGOHQ_URL'):
+    client = MongoClient(os.environ['MONGOHQ_URL'])
+    db = client.app31620493
+else:
+    client = MongoClient()
+    db = client.campusmap
+
 app = Flask(__name__)
 
-client = MongoClient()
-db = client.campusmap
+# client = MongoClient()
+# db = client.campusmap
 
 from app import views
