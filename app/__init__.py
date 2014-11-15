081298cb531a8731441c0244f0eb0db8fabd60ab
Kenzo Takahashi <kenzotakahashi2@gmail.com>
2014-11-15 07:42:31 -0600

    switched database

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from pymongo import MongoClient
 import os
 
-# Format: MONGOHQ_URL: mongodb://<user>:<pass>@<base_url>:<port>/<url_path>
-if os.environ.get('MONGOHQ_URL'):
-    client = MongoClient(os.environ['MONGOHQ_URL'])
-    db = client.app31620493
+# Format: MONGOLAB_URI: mongodb://<user>:<pass>@<base_url>:<port>/<url_path>
+if os.environ.get('MONGOLAB_URI'):
+    client = MongoClient(os.environ['MONGOLAB_URI'])
+    db = client.heroku_app31620493
 else:
     client = MongoClient()
     db = client.campusmap
